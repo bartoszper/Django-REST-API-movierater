8bb677233b4614c032366e5a67626d44101a9848
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 17:16:35 +0100

    Add function FilmViewSet

--- api/views.py
+++ api/views.py
@@ -11,6 +11,15 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    
+
+class FilmViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = FilmSerializer
+    
+
 
 
