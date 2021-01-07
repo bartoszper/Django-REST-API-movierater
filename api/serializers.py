2718b5ea00bc80d1998e87704dc6358a6f39754b
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 17:00:44 +0100

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
