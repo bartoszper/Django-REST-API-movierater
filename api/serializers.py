d7b8586902d27f1878fb9626c12acc497ec321f4
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 20:34:28 +0100

    Update

--- api/serializers.py
+++ api/serializers.py
@@ -8,14 +8,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'groups']
 
-class FilmSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Film
-        fields = ['id','tytul', 'opis', 'po_premierze', 'premiera','rok','imdb_rating',]
-
 class ExtraInfoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ExtraInfo
         fields = ['czas_trwania','rodzaj']
+
+
+class FilmSerializer(serializers.HyperlinkedModelSerializer):
+    extra_info = ExtraInfoSerializer(many=False)
+    class Meta:
+        model = Film
+        fields = ['id','tytul', 'opis', 'po_premierze', 'premiera','rok','imdb_rating','extra_info']
 
 
