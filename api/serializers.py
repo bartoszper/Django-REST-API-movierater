9d4306b78a2b32b87dd706d341927a729c69662c
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 21:02:46 +0100

    Add AktorSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Film, ExtraInfo
+from .models import Film, ExtraInfo, Recenzja, Aktor
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -14,10 +14,25 @@ class ExtraInfoSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['czas_trwania','rodzaj']
 
 
+class RecenzjaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Recenzja
+        fields = ['opis','gwiazdki']
+
+
+class AktorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Aktor
+        fields = ['imie','nazwisko','filmy']
+
+
 class FilmSerializer(serializers.HyperlinkedModelSerializer):
     extra_info = ExtraInfoSerializer(many=False)
+    recenzje = RecenzjaSerializer(many=True)
     class Meta:
         model = Film
-        fields = ['id','tytul', 'opis', 'po_premierze', 'premiera','rok','imdb_rating','extra_info']
+        fields = ['id','tytul', 'opis', 'po_premierze', 'premiera','rok','imdb_rating','extra_info','recenzje']
+
+
 
 
