599f2f357e3aa113f2ad0376ef1d981b2698c619
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 16:43:30 +0100

    Update

--- movierater/urls.py
+++ movierater/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
-from .api import views
+from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
