8eba2d951c5e5e0b9a0fcf88c794212c08570c09
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 20:07:48 +0100

    Update func

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Film
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'groups']
+
+class FilmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Film
+        fields = ['id','tytul', 'opis', 'po_premierze', 'premiera','rok','imdb_rating']
 
 
