f8326b3d96cebd9b7314cbee00d25e67f5a06ac4
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 17:16:09 +0100

    Create model "film"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Film(models.Model):
+    tytul = models.CharField(max_length=50)
+    opis = models.TextField()
+    po_premierze = models.BooleanField(default=False)
+    
