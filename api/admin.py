6f5aa5c53287aa1e6021f8c56e24e111f5e0bca4
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 22:32:22 +0100

    Add AktorAdmin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Film, ExtraInfo, Recenzja
+from .models import Film, ExtraInfo, Recenzja, Aktor
 
 class AdminFilm(admin.ModelAdmin):
     list_display = ('id', 'tytul','premiera','rok','imdb_rating')
@@ -7,8 +7,14 @@ class AdminFilm(admin.ModelAdmin):
     list_filter = ('id', 'tytul','premiera','rok')
     search_fields = ('id', 'tytul','premiera','rok')
 
+class AktorAdmin(admin.ModelAdmin):
+    list_display = ('imie','nazwisko')
+    list_display_links = ('imie','nazwisko')
+    list_filter = ('imie','nazwisko')
+    search_fields = ('imie','nazwisko')
+
 
 admin.site.register(Film, AdminFilm)
 admin.site.register(ExtraInfo)
 admin.site.register(Recenzja)
-admin.site.register(Aktor)
+admin.site.register(Aktor, AktorAdmin)
