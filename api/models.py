c371345a1f62776f7b0fe6351b52d2079e0d011d
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 20:07:36 +0100

    Create model ExtraInfo

--- api/models.py
+++ api/models.py
@@ -2,8 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class ExtraInfo(models.Model):
+    RODZAJE = {
+        (0, 'Nieznany'),
+        (1, 'Horror'),
+        (2, 'Sci-Fi'),
+        (3, 'Dramat'),
+        (5, 'Komedia'),
+    }
+
+    czas_trwania = models.IntegerField()
+    rodzaj = models.IntegerField(choices=RODZAJE, default=0)
 class Film(models.Model):
     tytul = models.CharField(max_length=50)
     opis = models.TextField()
     po_premierze = models.BooleanField(default=False)
-    
+    premiera = models.DateField(null=True, blank=True)
+    rok = models.IntegerField()
+    imdb_rating = models.DecimalField(decimal_places=2, max_digits=4 ,blank=True, null=True)
+    extra_ifno = models.OneToOneField(ExtraInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tytul
+
