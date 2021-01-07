79a33055ceccc9728c94e7474f272e79979fc0fe
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 20:08:06 +0100

    Update

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Film
 
-# Register your models here.
+class AdminFilm(admin.ModelAdmin):
+    list_display = ('id', 'tytul','premiera','rok','imdb_rating')
+    list_display_links = ('id','tytul')
+    list_filter = ('id', 'tytul','premiera','rok')
+    search_fields = ('id', 'tytul','premiera','rok')
+
+
+admin.site.register(Film, AdminFilm)
--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework import permissions
 from .serializers import UserSerializer
+from .models import Film
+from .serializers import FilmSerializer
+from rest_framework.response import Response
+from django.http.response import HttpResponseNotAllowed
+from rest_framework.decorators import action
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,9 +21,62 @@ class FilmViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = Film.objects.all()
     serializer_class = FilmSerializer
+
+    def create(self, request, *args, **kwargs):
+        #if request.user.is_staff:
+        film = Film.objects.create(tytul=request.data['tytul'], opis=request.data['opis'], po_premierze=request.data['po_premierze'])
+        serializer = FilmSerializer(film, many=False)
+        return Response(serializer.data)
+        #else:
+        #   return HttpResponseNotAllowed('Not allowed')
+
+    def update(self, request, *args, **kwargs):
+        film = self.get_object()
+        film.tytul = request.data['tytul']
+        film.opis = request.data['opis']
+        film.po_premierze = request.data['po_premierze']
+        film.save()
+        serializer = FilmSerializer(film, many=False)
+        return Response(serializer.data)
+
     
+    def destroy(self, request, *args, **kwargs):
+        film = self.get_object()
+        film.delete()
+        return Response('Film został usnięty')
+
+    #My own method
+
+    @action(detail=True)
+    def premiera(self, request, **kwargs):
+        film = self.get_object()
+        film.po_premierze = True
+        film.save()
+        serializer = FilmSerializer(film, many=False)
+        return Response(serializer.data)
+
+
+    @action(detail=False)
+    def premiera_all(self, request, **kwargs):
+        filmy = Film.objects.all()
+        filmy.update(po_premierze = True)
+    
+        serializer = FilmSerializer(filmy, many=True)
+        return Response(serializer.data)
+
+
+    @action(detail=False)
+    def przed_premiera_all(self, request, **kwargs):
+        filmy = Film.objects.all()
+        filmy.update(po_premierze = False)
+    
+        serializer = FilmSerializer(filmy, many=True)
+        return Response(serializer.data)
+
+
+
 
 
 
