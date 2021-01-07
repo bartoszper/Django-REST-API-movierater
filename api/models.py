cd02c96e7f54e5b5272158b67bca6ce290db025b
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-07 20:10:54 +0100

    bugfix

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ class Film(models.Model):
     premiera = models.DateField(null=True, blank=True)
     rok = models.IntegerField()
     imdb_rating = models.DecimalField(decimal_places=2, max_digits=4 ,blank=True, null=True)
-    extra_ifno = models.OneToOneField(ExtraInfo, on_delete=models.CASCADE)
+    extra_info = models.OneToOneField(ExtraInfo, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.tytul
