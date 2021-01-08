428f04bb1143690ab57f4ff14125fc4794465c91
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-08 13:42:36 +0100

    Add django_filters

--- movierater/settings.py
+++ movierater/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
     
     
 ]
@@ -126,4 +127,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
