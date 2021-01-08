a145f087d9c59167ea3503dde5fa74ab7f3e3e72
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-08 16:57:25 +0100

    Add token signal

--- api/models.py
+++ api/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
+
+@receiver(post_save, Sender = User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
 
 class ExtraInfo(models.Model):
     RODZAJE = {
